e2aa0a7d6915225b7b2ff1c04ab8f2ba1ee28d72
Max Ilalov <maximum_sill@mail.ru>
2019-12-15 22:55:52 +0300

    full game added

---
+++ src/UltimateTTTGame/Game/GameStart.java
@@ -0,0 +1,18 @@
+package UltimateTTTGame.Game;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameStart extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Ultimate Tic Tac Toe");
+        stage.setScene(new Scene(new TicTacToeGame(stage)));
+        stage.show();
+    }
+}
