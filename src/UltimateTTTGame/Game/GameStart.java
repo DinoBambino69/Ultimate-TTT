da8e6313a94cf95cc90d19741b8e1643af9493c2
DinoBambino69 <48675489+DinoBambino69@users.noreply.github.com>
2019-12-25 21:54:32 +0300

    jar add and all fixed

--- src/UltimateTTTGame/Game/GameStart.java
+++ src/UltimateTTTGame/Game/GameStart.java
@@ -1,8 +1,11 @@
 package UltimateTTTGame.Game;
 
+import UltimateTTTGame.Client.Client;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.HashMap;
 
 public class GameStart extends Application {
     public static void main(String[] args) {
